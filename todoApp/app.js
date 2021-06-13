d5b2e7a96a25cc8c97949ab469473bdcfa13ad25
sufyan arain <43380961+sufyanarain@users.noreply.github.com>
2021-06-13 21:44:31 +0500

    Updated input text suggestion

--- todoApp/app.js
+++ todoApp/app.js
@@ -45,7 +45,7 @@ addBtn.addEventListener("click", addBtnFunc)
 function addBtnFunc() {
     if (userInput.value === "") {
 
-        userInput.value = " "
+        userInput.value = "please input some text"
     } else {
         listUl.innerHTML += "<li id='listLi'>" + userInput.value + "<button id='deleteBtn' onclick='deleteFunc(this)''>Delete</button><button id='editBtn' onclick='editBtn11(this)'>Edit</button></li>";
         userInput.value = " "
