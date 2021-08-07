b6abfb97bc7a50716a258d065b7c52682a4b346f
sufyan arain <43380961+sufyanarain@users.noreply.github.com>
2021-08-07 15:19:51 -0700

    updated usermanagement project main page

--- userManagement/main.js
+++ userManagement/main.js
@@ -1,8 +1,9 @@
 
-var userName = document.getElementById("userName");
-var userEmail = document.getElementById("userEmail");
-var welcomeUser = document.getElementById("welcomeUser");
-var logOut = document.getElementById("logOut");
+let userName = document.getElementById("userName");
+let userEmail = document.getElementById("userEmail");
+let welcomeUser = document.getElementById("welcomeUser");
+let logOut = document.getElementById("logOut");
+let userLogedIn = document.getElementById("userLogedIn");
 
 
 
@@ -16,26 +17,53 @@ loginUsrObj = JSON.parse(loginUsrObj)
 
 // if(window.closed){
 
-    // }
-    
-    
-    if (!loginUsrObj) {
-        swal({
-            title: "Not Allowed",
-            text: "You are not logged in, please login first",
-            icon: "warning",
-            button: "OK",
-        })
-        .then((value) => {
-        window.location.href = "index.html"
+// }
 
-});
+
+if (!loginUsrObj) {
+    swal({
+        title: "Not Allowed",
+        text: "You are not logged in, please login first",
+        icon: "warning",
+        button: "OK",
+    })
+        .then((value) => {
+            window.location.href = "index.html"
+
+        });
     // document.body.innerHTML = `you are not logged in, please go to login page`
-    
+
 } else {
-    userName.innerHTML = `Name : ${loginUsrObj.userName}`
-    userEmail.innerHTML = `Email : ${loginUsrObj.email}`
-    welcomeUser.innerHTML = `Welcome : ${loginUsrObj.userName}`
+    // userName.innerHTML = `Name : ${loginUsrObj.userName}`
+    // userEmail.innerHTML = `Email : ${loginUsrObj.email}`
+    welcomeUser.innerHTML = `Welcome : ${loginUsrObj.userName}`;
+    userLogedIn.innerHTML = `You are now logged in`
+
+
+    // getting data from local storage
+    console.log(userData)
+    var userData = localStorage.getItem("userLocal1");
+    if (userData === null) {
+        var userData = []
+
+    } else {
+
+        userData = JSON.parse(userData)
+    }
+
+    let userTable = document.getElementById("userTable");
+
+    for (var i = 0; i < userData.length; i++) {
+        userTable.innerHTML += `
+         <tr>
+            <th scope="row">${i+1}</th>
+            <td>${userData[i].userName}</td>
+            <td>${userData[i].email}</td>
+         </tr>
+    `
+    }
+
+
 
     logOut.addEventListener('click', function () {
         localStorage.removeItem('loginObjD')
