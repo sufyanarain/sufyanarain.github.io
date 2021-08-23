66fef30aaf21335666853e3ad30a5be10e48ba11
sufyan arain <43380961+sufyanarain@users.noreply.github.com>
2021-08-23 07:58:51 -0700

    updated

--- teamreporter2/team.js
+++ teamreporter2/team.js
@@ -1,5 +1,5 @@
 
-
+// getting elements from dom
 let teamName = document.getElementById("teamName");
 let questionInp = document.getElementById("questionInp");
 let addQuestionBtn = document.getElementById("addQuestionBtn");
@@ -7,134 +7,103 @@ let questionUl = document.getElementById("questionUl");
 let teamMembers = document.getElementById("teamMembers");
 let membersUl = document.getElementById("membersUl");
 let membersLi = document.getElementsByClassName("membersLi");
-let membersLi2 = document.getElementsByClassName("membersLi2");
+let membersLiModal = document.getElementsByClassName("membersLiModal");
 let arrForMember = [];
 let showingMembers = [];
 
+// getting data from localStorage
+let curruntUser = localStorage.getItem("userObjLocal");
+curruntUser = JSON.parse(curruntUser);
+
+let userIndex = localStorage.getItem("userIndex");
+userIndex = JSON.parse(userIndex);
+
+let teamIndex = localStorage.getItem("teamIndex");
+teamIndex = JSON.parse(teamIndex);
+
+let curruntTeam = curruntUser[userIndex].createdTeam[teamIndex];
+// function for reloading page
 function refresh() {
     setTimeout(function () {
         location.reload()
     }, 100);
 }
 
+// function for deleting members 
 let deletMemberFunc = (e, eId) => {
-    let userL = localStorage.getItem("userObjLocal");
-    userL = JSON.parse(userL);
-    let userIndex = localStorage.getItem("userIndex");
-    userIndex = JSON.parse(userIndex);
-
-    let teamIndex = localStorage.getItem("teamIndex");
-    teamIndex = JSON.parse(teamIndex);
+    // deleting member from dom
     e.parentNode.remove();
-    console.log(e.parentNode);
-    // e.parentNode.remove()
 
-    userL[userIndex].createdTeam[teamIndex].members.splice(eId, 1)
-    // arrForMember.push(userL[eId].name);
+    // deleting element from user object
+    curruntTeam.members.splice(eId, 1)
 
-    // userL[userIndex].createdTeam[teamIndex].members.push(userL[eId].name);
-    console.log(userL);
-    localStorage.setItem("userObjLocal", JSON.stringify(userL))
+    // setting data to local storage after deleting member
+    localStorage.setItem("userObjLocal", JSON.stringify(curruntUser))
     refresh()
-
 }
 
+// displying team name and member 
 let displayFunc = () => {
-    let userL = localStorage.getItem("userObjLocal");
-    userL = JSON.parse(userL);
-    let userIndex = localStorage.getItem("userIndex");
-    userIndex = JSON.parse(userIndex);
 
-    let teamIndex = localStorage.getItem("teamIndex");
-    teamIndex = JSON.parse(teamIndex);
-    console.log(teamIndex);
+    // showing team name
+    teamName.innerHTML = `Team Name : ${curruntTeam.teamName}`;
 
-
-    teamName.innerHTML = `Team Name : ${userL[userIndex].createdTeam[teamIndex].teamName}`;
-
-    for (let i = 0; i < userL[userIndex].createdTeam[teamIndex].members.length; i++) {
+    //    showing team members on main page
+    for (let i = 0; i < curruntTeam.members.length; i++) {
         teamMembers.innerHTML += `
-        <li class="membersLi" id="${i}">${userL[userIndex].createdTeam[teamIndex].members[i]}<i id="${i}" onclick="deletMemberFunc(this,this.id)" class="bi bi-x-circle-fill "></i></li>`
-        showingMembers.push(userL[userIndex].createdTeam[teamIndex].members[i])
+        <li class="membersLi" id="${i}">${curruntTeam.members[i]}<i id="${i}" onclick="deletMemberFunc(this,this.id)" class="bi bi-x-circle-fill "></i></li>`
+        showingMembers.push(curruntTeam.members[i])
     }
 
 }
 displayFunc()
 
-console.log(showingMembers)
+// function for adding question to object
 let addQuestFunc = () => {
-    let userL = localStorage.getItem("userObjLocal");
-    userL = JSON.parse(userL);
-    let userIndex = localStorage.getItem("userIndex");
-    userIndex = JSON.parse(userIndex);
-
-    let teamIndex = localStorage.getItem("teamIndex");
-    teamIndex = JSON.parse(teamIndex);
-    console.log(teamIndex);
-
-    if (userL[userIndex].createdTeam[teamIndex].questions === undefined) {
+    // getting questions from users input
+    if (curruntTeam.questions === undefined) {
         var questArr = []
     } else {
 
-        questArr = userL[userIndex].createdTeam[teamIndex].questions;
+        questArr = curruntTeam.questions;
     }
     questArr.push(questionInp.value)
-    userL[userIndex].createdTeam[teamIndex].questions = questArr;
-    localStorage.setItem("userObjLocal", JSON.stringify(userL))
-    console.log(localStorage.getItem("usersClickedTeam"))
+    // setting question in  users object 
+    curruntTeam.questions = questArr;
+    // updating users object after adding question
+    localStorage.setItem("userObjLocal", JSON.stringify(curruntUser))
 
+    // removing question from input afer adding
     questionInp.value = ""
     displayQuestFunc()
     // refresh()
 }
 addQuestionBtn.addEventListener('click', addQuestFunc);
 
-
+// function for deleting questions
 let deleteQuest = (id, e) => {
+    // deleting question from dom
     e.parentNode.parentNode.remove()
 
-    let userL = localStorage.getItem("userObjLocal");
-    userL = JSON.parse(userL);
-    let userIndex = localStorage.getItem("userIndex");
-    userIndex = JSON.parse(userIndex);
-
-    let teamIndex = localStorage.getItem("teamIndex");
-    teamIndex = JSON.parse(teamIndex);
-    console.log(teamIndex);
-
-    userL[userIndex].createdTeam[teamIndex].questions.splice(id, 1);
-    localStorage.setItem("userObjLocal", JSON.stringify(userL))
+    // deleting question from users object
+    curruntTeam.questions.splice(id, 1);
+    // setting localstorage updated object after deleting question
+    localStorage.setItem("userObjLocal", JSON.stringify(curruntUser))
 
 }
 
+// function for displaying questions
 let displayQuestFunc = () => {
     questionUl.innerHTML = ""
-
-    let userL = localStorage.getItem("userObjLocal");
-    userL = JSON.parse(userL);
-    let userIndex = localStorage.getItem("userIndex");
-    userIndex = JSON.parse(userIndex);
-
-    let teamIndex = localStorage.getItem("teamIndex");
-    teamIndex = JSON.parse(teamIndex);
-    console.log(teamIndex);
-
-    // if(userL[userIndex].createdTeam[teamIndex].questions === undefined){
-    //     userL[userIndex].createdTeam[teamIndex].questions = []
-    // }else{
-
-    // console.log(userL[userIndex].createdTeam[teamIndex])
-
-    for (let i = 0; i < userL[userIndex].createdTeam[teamIndex].questions.length; i++) {
+    // loop for displaying question
+    for (let i = 0; i < curruntTeam.questions.length; i++) {
         questionUl.innerHTML += `
-                <li id="${i}" class="list-group-item"><div>${userL[userIndex].createdTeam[teamIndex].questions[i]}</div><div><i id="${i}" onclick="deleteQuest(this.id,this)" class="bi bi-trash"></i></div></li>`
+                <li id="${i}" class="list-group-item"><div>${curruntTeam.questions[i]}</div><div><i id="${i}" onclick="deleteQuest(this.id,this)" class="bi bi-trash"></i></div></li>`
     }
-
-
-    console.log(userL[userIndex].createdTeam[teamIndex].questions)
 
 }
 
+// reloading page after adding members
 let addMembersFunc = () => {
     refresh()
 
@@ -142,88 +111,41 @@ let addMembersFunc = () => {
 
 // function showing useres and add them
 let addMemberFunc = (e, eId) => {
-    let userL = localStorage.getItem("userObjLocal");
-    userL = JSON.parse(userL);
-    let userIndex = localStorage.getItem("userIndex");
-    userIndex = JSON.parse(userIndex);
-
-    let teamIndex = localStorage.getItem("teamIndex");
-    teamIndex = JSON.parse(teamIndex);
-    console.log(teamIndex);
+    // removing members from dom after clicking on members
     e.remove()
-    arrForMember.push(userL[eId].name);
+    // pushing members after clicking on member
+    arrForMember.push(curruntUser[eId].name);
 
-    userL[userIndex].createdTeam[teamIndex].members.push(userL[eId].name);
-    console.log(userL);
-    localStorage.setItem("userObjLocal", JSON.stringify(userL))
+    // setting data in object after adding
+    curruntTeam.members.push(curruntUser[eId].name);
+    // setting data to localstorage after updating members
+    localStorage.setItem("userObjLocal", JSON.stringify(curruntUser))
 }
+
+// showing members in modal box
 let addMembers = () => {
-    // getting main user's data from local storage
-    let userL = localStorage.getItem("userObjLocal");
-    userL = JSON.parse(userL);
-
-    
-    for (let i = 0; i < userL.length; i++) {
-
+    for (let i = 0; i < curruntUser.length; i++) {
         membersUl.innerHTML += `
-        <li class="membersLi2" onclick="addMemberFunc(this,this.id)"  id="${i}">${userL[i].name}</li>`
+        <li class="membersLiModal" onclick="addMemberFunc(this,this.id)"  id="${i}">${curruntUser[i].name}</li>`
+
         for (let s = 0; s < membersLi.length; s++) {
-            if (membersLi2[i].innerText == membersLi[s].innerText) {
-                membersLi2[i].style.display = "none"
+            if (membersLiModal[i].innerText == membersLi[s].innerText) {
+                membersLiModal[i].style.display = "none"
             }
         }
     }
-
-    // console.log(membersLi[0].innerHTML)
-
 }
 addMembers()
 
 
 // delete team function
 let deleteTeamFunc = () => {
-    // getting data from localstorage
-    let userL = localStorage.getItem("userObjLocal");
-    userL = JSON.parse(userL);
-    
-    let userIndex = localStorage.getItem("userIndex");
-    userIndex = JSON.parse(userIndex);
-
-    let teamIndex = localStorage.getItem("teamIndex");
-    teamIndex = JSON.parse(teamIndex);
     // getting index from id and deleting from object
-    userL[userIndex].createdTeam.splice(teamIndex, 1)
+    curruntUser[userIndex].createdTeam.splice(teamIndex, 1)
     // setting deleted item from object and setting it to main object
-    localStorage.setItem("userObjLocal", JSON.stringify(userL));
+    localStorage.setItem("userObjLocal", JSON.stringify(curruntUser));
+    // changing window location
     window.location = "main.html"
-
 }
-
-// swal({
-//     title: "Are you sure?",
-//     text: "Once deleted, you will not be able to recover this imaginary file!",
-//     icon: "warning",
-//     buttons: true,
-//     dangerMode: true,
-//   })
-//   .then((willDelete) => {
-//     if (willDelete) {
-//       swal(console.log(""), {
-//         icon: "success",
-//       });
-//     } else {
-//       swal("Your imaginary file is safe!");
-//     }
-//   });
-  
-
-
-
-
-
-
-
-
-
 
 displayQuestFunc()
