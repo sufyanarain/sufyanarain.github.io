86b844fe90b1d6577f867d2a503b7c188f7b155f
sufyan arain <43380961+sufyanarain@users.noreply.github.com>
2021-09-18 10:47:25 -0700

    updated

--- foodDelApp/dashboard.js
+++ foodDelApp/dashboard.js
@@ -13,23 +13,21 @@ firebase.initializeApp(firebaseConfig);
 let auth = firebase.auth();
 let database = firebase.firestore();
 
-let dashboardDisplay = document.getElementById("dashboardDisplay");
+let pending = document.getElementById("pending");
 
+let uid;
 auth.onAuthStateChanged((user) => {
-    console.log(user.uid)
-    // database.collection("orders").get().then((snap)=>{
-    //     snap.forEach(element => {
-    //         console.log(element.data())
-    //     });
-    //     console.log(snap)
-    // })
-    database.collection("orders").where("restaurantKey", "==", user.uid).get().then((snap) => {
+    uid = user.uid;
+    // console.log(user.uid)
+
+
+    database.collection("orders").where("restaurantKey", "==", uid).get().then((snap) => {
         snap.forEach(element => {
             database.collection("users").doc(element.data().customerUid).get().then((elem) => {
-                console.log(element.data())
+                // console.log(element.data())
                 database.collection("items").doc(element.data().itemKey.toString()).get().then((item) => {
 
-                    dashboardDisplay.innerHTML += `<div class="card mt-2">
+                    pending.innerHTML += `<div class="card mt-2">
                     <div class="card-body">
                     <h6 class="card-title">User : ${elem.data().userName}</h6>
                     <hr>
@@ -39,19 +37,50 @@ auth.onAuthStateChanged((user) => {
                     <li class="list-group-item">Price : ${item.data().itemPrice} Rs</li>
                     <li class="list-group-item">Quantity : ${element.data().numOfOrder}</li>
                     <li class="list-group-item"><i class="bi bi-truck"></i>  ${item.data().deliveryTypeCategory}</li>
-                    <li class="list-group-item">Total Price : `+item.data().itemPrice * element.data().numOfOrder+
-                    `</li>
+                    <li class="list-group-item">Total Price : `+ item.data().itemPrice * element.data().numOfOrder +
+                        `</li>
                     </ul>
                     <div class="accepRejBtn card-body">
-                    <button class="btn btn-outline-primary">Accept</button>
-                    <button class="btn btn-outline-danger">Reject</button>
+                    <button onclick='acceptFunc("${elem.data().userName}","${item.data().itemName}"," ${item.data().itemPrice}","${element.data().numOfOrder}","${item.data().deliveryTypeCategory}",this,"${element.id}")' class="btn btn-outline-primary">Accepted</button>
+                    <button onclick='rejectFunc(this,"${element.id}")' class="btn btn-outline-danger">Reject</button>
                     </div>
                     </div>
                     `
-                    console.log(element.data())
+                    // console.log(element.id)
                 })
             })
 
         });
     })
 })
+
+let acceptFunc = (userName, itemName, itemPrice, numOfOrder, deliveryTypeCategory, e, elementId) => {
+    database.collection("acceptedOrders").add({
+        userName, itemName, itemPrice, numOfOrder, deliveryTypeCategory
+    })
+    database.collection("orders").doc(elementId).delete().then(() => {
+        console.log("deleted")
+    }).catch((error) => {
+        console.log(error.message)
+    })
+    e.parentNode.parentNode.remove()
+    // console.log(elementId)
+}
+
+let rejectFunc = (e, elementId) => {
+    database.collection("orders").doc(elementId).delete().then(() => {
+        console.log("deleted")
+    }).catch((error) => {
+        console.log(error.message)
+    })
+    e.parentNode.parentNode.remove()
+}
+
+// *********** accepted orders *************
+// let accepted = document.getElementById("accepted");
+
+// auth.onAuthStateChanged((user) => {
+//     database.collection("acceptedOrders").where()
+
+// })
+
